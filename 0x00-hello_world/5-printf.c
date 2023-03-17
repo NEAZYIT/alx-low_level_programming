41a186fe76331ab56aa3f2a58f5a13e538df0682
NEAZYIT <touihnazih28@gmail.com>
2023-03-16 19:44:52 -0700

    prin

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /*
- *  * main - Entry point
- *   * Description: This program uses printf to print a message to the console.
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * Description: This program uses printf to print a message to the console.
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
-{
-	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	        return (0);
+{	
+       	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
