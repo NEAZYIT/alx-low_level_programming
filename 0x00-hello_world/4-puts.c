40323f5b429509415a5b8bf623ff63a25e912252
NEAZYIT <touihnazih28@gmail.com>
2023-03-16 18:57:21 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: this my first program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
