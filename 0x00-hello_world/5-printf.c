53a81ef1b480180d1d649b5c7f9b060ce9b3c874
NEAZYIT <touihnazih28@gmail.com>
2023-03-16 19:35:26 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ *  main - Entry point 
+ *  Descriptio: this program used printf
+ *
+ *  Return Always 0 (Succsess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);	
+}
