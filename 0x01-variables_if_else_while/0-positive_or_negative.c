248697a32f3d6233b795bab0040283a73dffc0fa
NEAZYIT <touihnazih28@gmail.com>
2023-03-17 18:32:54 -0700

    IF

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,9 +22,9 @@ int main(void)
 	if(n>0){
 	printf("is positive\n");
 	} else if(n==0){
-	printf("is negative\n");
-	} else {
 	printf("is zero\n");
+	} else {
+	printf("is negative\n");
 	}
 	return (0);
 }
