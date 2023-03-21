8cbed7ab72db306a80dc550864c2045b3d52cfc5
NEAZYIT <touihnazih28@gmail.com>
2023-03-21 12:45:57 -0700

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,19 +6,17 @@
  * Return: Always 0.
  */
 
-
-int main (void)
+int main(void)
 {
-    char str[9] = "_putchar";   
-
+    char str[9] = "_putchar";
     int i = 0;
 
     while ("_putchar"[i] != '\0')
     {
         putchar(str[i]);
-        
         i++;
     }
+
     putchar('\n');
-    return 0;
+    return (0);
 }
