58543e8e80c051e5d3dd349a839dcb88b0265b4c
NEAZYIT <touihnazih28@gmail.com>
2023-03-21 13:04:51 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+
+int main (void)
+{
+char str[27] = "abcdefghijklmnopqrstuvwxyz";
+
+int i = 0;
+
+while ("abcdefghijklmnopqrstuvwxyz"[i] != '\0')
+{
+	putchar(str[i]);
+
+        i++;
+}
+putchar('\n');
+return 0;
+}
