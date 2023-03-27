e61bd8a4a9535b552bf9bf50c31c32915c45b322
NEAZYIT <touihnazih28@gmail.com>
2023-03-27 16:54:36 -0700

    lengtsize

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,11 +7,14 @@
  * Description: this will return the length of a string
  * Return: 0 is success
  */
-int _strlen(char *s) {
+int _strlen(char *s)
+{
     int len = 0;
-    while (*s != '\0') {
-        len++;
-        s++;
+    while (*s != '\0')
+    {
+               len++;
+               s++;
     }
+
     return len;
 }
