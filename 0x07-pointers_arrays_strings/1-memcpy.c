b885f17a10c3bb6b8c0bcbaa05713e660ba0f50e
NEAZYIT <touihnazih28@gmail.com>
2023-04-03 18:38:35 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-/* loop through each byte to copy */
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i]; /* copy each byte from source to destination */
-	}
-	return (dest); /* return pointer to destination memory area */
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
 }
