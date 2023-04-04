e425a55dbc71253ec709da92362895ff9e4c61f7
NEAZYIT <touihnazih28@gmail.com>
2023-04-03 19:33:20 -0700

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,9 +16,8 @@ int diagonal1 = 0;
 int diagonal2 = 0;
 
 for (s = 0; s < size; s++)
-{
-	diagonal1 += a[(size + 1) * s];
-    diagonal2 += a[(size - 1) * (s + 1)];
-}
+diagonal1 = diagonal1 + a[(size + 1) * s];
+for (s = 1; s < size + 1; s++)
+diagonal2 = diagonal2 + a[(size - 1) * s];
 printf("%d, %d\n", diagonal1, diagonal2);
 }
