06c09291b1e023f542e45d26f89eb7b62d489083
NEAZYIT <touihnazih28@gmail.com>
2023-04-19 18:06:36 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,40 +2,43 @@
 /**
  * main - performs simple operations
  *
- * @argc: number of arguments passed to the program
- * @argv: array of arguments passed to the program
+ * @argv: pointer to the argument that enter from the user as char type
+ * @argc: tell us the amount of arguments int type
  *
- * Return: 0 on success, otherwise an error code
+ * Return: an integer with the result of a operation
  */
-int main(int argc, chat *argv[])
+int main(int argc, char *argv[])
 {
 	int (*funoperator)(int a, int b);
-
-	/* Check if the nulber of arguments is correct */
+	/* when the user enter the arguments there´s must be four elements */
+	/* the name of the file + first number + operator + second number */
+	/* follow first condition of the task */
+	/* if the number or arguments is wrong, print error */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	/* Get the function pointer for the operator passed as argument */
-	funoperator = gte_op_func(argc[2]);
-
-	/* Check if the operator is valid */
+	/* funoperator is gonna be equal to the pointer function */
+	/* in the argument number 2 that is the position of the operator */
+	/*if is not one of the operators mentioned else print Error */
+	funoperator = get_op_func(argv[2]);
 	if (!funoperator || strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	/* Check for division by zero */
+	/* add last condition, if user try to divides using % or / is Error */
+	/* that means, if *argv in position 2 that are operators */
+	/* receives % or / operators program prints Error */
+	/* AND if user is trying to divided by 0 is Error too */
+	/* e.g 2  % 0 , if argv in position 3is equal to cero is Error */
 	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	/* print the result of the operation */
+	/* if any of the conditions theeennn : */
 	printf("%d\n", funoperator(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
