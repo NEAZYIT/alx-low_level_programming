d1cc3d51d4df0d71851d27850bf08c8463190d07
NEAZYIT <touihnazih28@gmail.com>
2023-04-19 17:52:44 -0700

    THE 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ *
+ * @argc: counts the number of arguments
+ * @argv: the arguments that enters to the program
+ *
+ * Return: the opcode hex number
+ */
+
+int main(int argc, char **argv)
+{
+	int runner = 0, bytes;
+	/* the tasks tell us each opcode is two char long */
+	/* if the argv is less or more then Error */
+
+	if (argv != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	/* the argument in position one (the number the program recieves) */
+	/* will be equal to bytes */
+	bytes = atoi(argv[1]);
+	/* condition if the number of bytes y negative (less than zero) */
+	/* exit status 2 and print Error */
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	/* we use runner to  go through the array we recieve in  the argv */
+	while (runner < bytes)
+	{
+		/* is pre define how to print an opcodes */
+		printf("%02hhx", *((unsigned char *)(main + runner)));
+		if (bytes > runner + 1)
+			printf(" ");
+		runner++;
+	}
+	printf("\n");
+	return (0);
+}
