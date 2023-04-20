041058d5e0aafb348425c35076cbbe26c3847a6c
NEAZYIT <touihnazih28@gmail.com>
2023-04-19 17:29:54 -0700

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ *
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments passed to the program
+ *
+ * Return: 0 on success, otherwise an error code
+ */
+
+int main(int argc, chat *argv[])
+{
+	int (*funoperator)(int a, int b);
+
+	/* Check if the nulber of arguments is correct */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (EXIT_FAILURE);
+	}
+
+	/* Get the function pointer for the operator passed as argument */
+	funoperator = gte_op_func(argc[2]);
+
+	/* Check if the operator is valid */
+	if (!funoperator || strlen(argv[2]) != 1)
+	{
+		printf("Error\n");
+		return (EXIT_FAILURE);
+	}
+
+	/* Check for division by zero */
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		return (EXIT_FAILURE);
+	}
+
+	/* print the result of the operation */
+	printf("%d\n", funoperator(atoi(argv[1]), atoi(argv[3])));
+
+	return (EXIT_SUCCESS);
+}
