39372ff9df801f48d4277614c76b2e38a9ad63f8
NEAZYIT <touihnazih28@gmail.com>
2023-09-08 11:32:08 -0700

    code is a C function called hash_table_create, which is used to create a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,28 +1,27 @@
 #include "hash_tables.h"
+
 /**
-* hash_table_create - create a new hash table
-* @size: size of the array
-* Return: a pointer to the new hash table
-*/
+ * hash_table_create - function to create a hash table
+ * @size: Size of the array
+ * Return: Null for error or a pointer to a new hash table
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int index = 0;
-	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
+	unsigned long int i;
 
-	if (!new_hash_table)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
-	new_hash_table->array = malloc(sizeof(hash_node_t **) * size);
-	if (!new_hash_table->array)
-	{
-		free(new_hash_table);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
-	}
-	while (index < size)
+	for (i = 0; i < size; ++i)
 	{
-		new_hash_table->array[index] = NULL;
-		index++;
+		ht->array[i] = NULL;
 	}
-	new_hash_table->size = size;
-	return (new_hash_table);
+	return (ht);
 }
