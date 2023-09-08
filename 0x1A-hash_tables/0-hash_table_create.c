49e0fad9d6feb0b17592522016d456faa4872169
NEAZYIT <touihnazih28@gmail.com>
2023-09-08 11:26:54 -0700

    code is a C function called hash_table_create, which is used to create a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,34 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: the size of the hash table
- * Return: a pointer to the newly created hash table.
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *hash_table;
+	unsigned long int index; /* Used for iterating through the array */
 
-	if (size == 0)
+	/* Allocate memory for the hash table structure */
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
 
-	new_table = calloc(1, sizeof(hash_table_t));
-	if (new_table == NULL)
+	/* Initialize the size of the hash table */
+	hash_table->size = size;
+
+	/* Allocate memory for the array of hash nodes */
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
 		return (NULL);
 
-	new_table->size = size;
-	new_table->array = calloc(size, sizeof(hash_table_t *));
-	if (new_table->array == NULL)
-	{
-		free(new_table);
-		return (NULL);
-	}
+	/* Initialize each slot in the array to NULL */
+	for (index = 0; index < size; index++)
+		hash_table->array[index] = NULL;
 
-	return (new_table);
+	return (hash_table);
 }
