86f538733910600b624c39f5fb8f542088e9212c
NEAZYIT <touihnazih28@gmail.com>
2023-10-25 12:14:37 -0700

    Refactored 5-island_perimeter.py to fix block comment and line length issues.

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,21 +11,14 @@ def island_perimeter(grid):
         int: The perimeter of the island.
 
     """
-    perimeter = 0
-
-    # Define the directions to check around the cell
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                perimeter += 4  # Start with the maximum perimeter
-
-                # Check each direction and subtract 1 from the perimeter
-                # for each neighboring land cell
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == 1:
-                        perimeter -= 1
-
-    return perimeter
+    lands = 0
+    side = 0
+    for height in range(len(grid)):
+        for width in range(len(grid[height])):
+            if grid[height][width] == 1:
+                lands += 1
+                if height > 0 and grid[height-1][width] == 1:
+                    side += 1
+                if width > 0 and grid[height][width-1] == 1:
+                    side += 1
+    return lands * 4 - side * 2
