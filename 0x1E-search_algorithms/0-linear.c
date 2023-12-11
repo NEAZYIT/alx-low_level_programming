96e8fb9e81b16a0c9c41e4e1e25ffbdcde9d0ff0
NEAZYIT <touihnazih28@gmail.com>
2023-12-11 17:13:17 +0100

    Adjusted the 'linear_search' function to ensure proper iteration through the array for value comparison. This update rectifies the logic to avoid prematurely returning -1 and correctly identifies the index of the sought value. Additionally, fixed a format specifier in a printf statement by casting 'size_t' to 'unsigned long' to resolve potential format mismatch warnings during compilation.~

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches for a value in an array of integers
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in array
+ * @value: Value to search for
+ *
+ * Return: First index where value is located
+ * or -1 if value is absent from array or if array is NULL
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+    if (!array)
+    {
+        return (-1);
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
+        if (array[i] == value)
+        {
+            return (i);
+        }
+    }
+
+    return (-1);
+}
